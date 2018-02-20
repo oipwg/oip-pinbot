844720be82738c21228008fb9c0198820c7f3076
OstlerDev <OstlerDev@users.noreply.github.com>
2018-02-20 13:21:48 -0800

    Initial commit

---
+++ PinWizard.js
@@ -0,0 +1,79 @@
+module.exports =
+class PinWizard {
+	constructor(OIPJS) {
+		this.OIPJS_ = OIPJS
+	}
+
+	updateClusterPins(onSuccess, onError) {
+		var _this = this;
+		this.getAllIPFSLocations(function(locations){
+			_this.getUnpinned(locations, function(unpinned){
+				var totalPinned = locations.length - unpinned.length, newPins = 0, unableToPin = 0;
+
+				var tryFinish = function(successful){
+					if (successful)
+						newPins++;
+					else
+						unableToPin++;
+
+					if (totalPinned + newPins + unableToPin === locations.length)
+						onSuccess(locations.length, totalPinned + newPins, newPins, unableToPin);
+				}
+
+				for (var loc of unpinned){
+					_this.OIPJS_.Network.ipfsAPIPin(loc, function(err, info){
+						if (err){
+							console.error(err);
+							tryFinish(false);
+						} else {
+							tryFinish(true);
+						}
+					})
+				}
+
+				if (unpinned.length === 0)
+					onSuccess(locations.length, totalPinned, newPins, unableToPin);
+			}, onError)
+		}, onError)
+	}
+
+	getUnpinned(locations, onSuccess, onError){
+		this.OIPJS_.Network.ipfsClusterAPI.pin.ls(function(err, pinset){
+			if (err){
+				onError(err);
+			} else {
+				var pinnedLocs = [];
+
+				for (var pin of pinset){
+					pinnedLocs.push(pin.hash);
+				}
+
+				var unpinned = [];
+
+				for (var loc of locations){
+					if (pinnedLocs.indexOf(loc) === -1)
+						unpinned.push(loc);
+				}
+
+				onSuccess(unpinned);
+			}
+		})
+	}
+
+	getAllIPFSLocations(onSuccess, onError){
+		var _this = this;
+
+		this.OIPJS_.Index.getSupportedArtifacts(function(artifacts){
+			var IPFSHashes = [];
+
+			for (var artifact of artifacts){
+				var hash = _this.OIPJS_.Artifact.getLocation(artifact);
+
+				if (IPFSHashes.indexOf(hash) === -1)
+					IPFSHashes.push(hash)
+			}
+
+			onSuccess(IPFSHashes)
+		}, onError)
+	}
+}
---
+++ index.js
@@ -0,0 +1,22 @@
+var OIPJS = require('oip-js');
+
+var PinWizard = require('./PinWizard.js');
+
+var UpdatePins = function(){
+	var wiz = new PinWizard(OIPJS);
+
+	wiz.updateClusterPins(function(overallPins, totalPinned, newPins, unableToPins){
+		console.log("Pinned " + totalPinned + "/" + overallPins + " | " + newPins + " new Pins | Unable to pin " + unableToPins);
+	}, function(error){
+		console.error(error);
+	});
+}
+
+var PinIntervalMinutes = 30;
+const SECONDS = 60
+const MILISECONDS = 1000
+
+var PinInterval = setInterval(UpdatePins, PinIntervalMinutes * SECONDS * MILISECONDS);
+
+// Run on startup as well.
+UpdatePins();
